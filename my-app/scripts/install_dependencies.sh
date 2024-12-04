f74ec6979f70f46c58cf6551a14b912ad312d3b8
shivani2029 <helloshivi123@gmail.com>
2024-12-04 18:48:57 +0530

    new

--- my-app/scripts/install_dependencies.sh
+++ my-app/scripts/install_dependencies.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-# mkdir /home/ubuntu/react-app
-# cd /home/ubuntu/react-app
-# npm install
-# npm run build
-set -e
-mkdir -p /home/ubuntu/react-app
-chown -R ubuntu:ubuntu /home/ubuntu/react-app
-
-# after_install.sh
-#!/bin/bash
-set -e
+sudo apt update
+mkdir /home/ubuntu/react-app
 cd /home/ubuntu/react-app
 npm install
 
-# stop_server.sh
-#!/bin/bash
-pkill -f "npm start" || true
-pkill -f "node" || true

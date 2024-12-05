2244986f1f53a9eff452b4f58d1942fde9141b83
shivani2029 <helloshivi123@gmail.com>
2024-12-05 16:47:31 +0530

    new

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/my-app
 
 # Stop existing PM2 process if any
-pm2 delete react-app || true
+pm2 delete my-app || true
 
 # Start application using PM2
 pm2 start npm --name my-app -- start

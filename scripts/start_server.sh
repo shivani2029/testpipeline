3e22293ba6c0e39993a1bdd9a524fdf9a6967dcc
shivani2029 <helloshivi123@gmail.com>
2024-12-05 14:14:38 +0530

    new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/my-app
+cd /home/ubuntu/my-app || exit 1
 # npx serve -s build -l 3000
-sudo npm run build
-serve -s build -l 3000 &
+npm run build
+sudo serve -s build -l 3000 &
 
 
 
